1262881409c03e410b684575b4186336baa0ad53
Mustafa Yasin <mustafa.yasin2404@gmail.com>
2021-12-23 23:30:19 +0100

    dynamicprogramming

---
+++ dynamicprogramming/max_subset_sum_no_adjacent.py
@@ -0,0 +1,13 @@
+def maxSubsetSumNoAdjacent(array):
+	if len(array) == 0:
+		return 0
+	if len(array) == 1:
+		return array[0]
+	else:
+		firstSum = array[0]
+		secondSum = max(array[0], array[1])
+		#sumArray = array[:]
+		for i in range(2, len(array)):
+			result = max(secondSum, firstSum + array[i])
+			firstSum, secondSum = secondSum, result
+		return secondSum
