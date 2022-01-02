9f25a2c2c36b7f113bce0506a811cbf1f5545bb7
Mustafa Yasin <mustafa.yasin2404@gmail.com>
2022-01-02 19:14:08 +0100

    linkedlist

---
+++ linkedlist/remove_duplicates_from_linkedlist.py
@@ -0,0 +1,18 @@
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def removeDuplicatesFromLinkedList(linkedList):
+	currentNode = linkedList
+	while currentNode:
+		temp = currentNode.next
+		while temp is not None and temp.value == currentNode.value:
+			temp = temp.next
+		
+		currentNode.next = temp
+		currentNode = temp
+		
+		
+    return linkedList
