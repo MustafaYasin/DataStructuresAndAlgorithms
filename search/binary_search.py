dd8dea4483843a6f4e669b2da75d427691180e07
Mustafa Yasin <mustafa.yasin2404@gmail.com>
2022-01-17 20:54:01 +0100

    binary  search recursive solution

--- search/binary_search.py
+++ search/binary_search.py
@@ -3,7 +3,7 @@
 ##############################################################
 
 def binarySearch(array, target):
-    # Write your code here.
+
 	leftPointer = 0
 	rightPointer = len(array) - 1
 	
@@ -25,3 +25,26 @@ def binarySearch(array, target):
 ##############################################################
 # This is a recursive solution to the binary search problem  #
 ##############################################################
+
+
+
+def binarySearch(array, target):
+    return helperBinarySearch(array, target, 0,len(array)-1)
+
+
+def helperBinarySearch(array, target, leftPointer, rightPointer):
+	
+	while leftPointer <= rightPointer:
+		midPointer = (leftPointer + rightPointer) // 2
+		
+		if target == array[midPointer]:
+			return midPointer
+		elif target > array[midPointer]:
+			return helperBinarySearch(array, target, midPointer + 1, rightPointer)
+		else:
+			return helperBinarySearch(array, target, leftPointer, midPointer - 1)
+		
+	return - 1
+
+	
+	
