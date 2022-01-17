4ebbabe4f8bc71fcfa92f85ed2856864b78e0b90
Mustafa Yasin <mustafa.yasin2404@gmail.com>
2022-01-17 20:35:54 +0100

    binary search

---
+++ search/binary_search.py
@@ -0,0 +1,22 @@
+##############################################################
+# This is an iterative solution to the binary search problem #
+##############################################################
+
+def binarySearch(array, target):
+    # Write your code here.
+	leftPointer = 0
+	rightPointer = len(array) - 1
+	
+	while leftPointer <= rightPointer:
+		midPointer = (rightPointer + leftPointer) // 2
+		
+		if target == array[midPointer]:
+			return midPointer
+		
+		elif target > array [midPointer]:
+			leftPointer = midPointer + 1
+		
+		else:
+			rightPointer = midPointer - 1		
+		
+	return - 1
